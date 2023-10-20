c45db2f6d9ee7140bdc82f4751b99314623933b9
EstebanPastor <estebanandrespastor@gmail.com>
2023-10-20 12:13:02 -0300

    style: :art: Add parallax wallpaper and subscribe to our newsletter section/button

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import Features from "./components/ui/features/Features";
 import Hero from "./components/ui/header/Hero";
+import Parallax from "./components/ui/parallax/Parallax";
 
 export default function Home() {
-  return <>
-    <Hero />
-    <Features />
-  </>;
+  return (
+    <>
+      <Hero />
+      <Features />
+      <Parallax />
+    </>
+  );
 }
