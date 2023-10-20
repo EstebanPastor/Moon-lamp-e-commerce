f2a751540704df40dfaf175d971003ca8d29e36d
EstebanPastor <estebanandrespastor@gmail.com>
2023-10-20 17:52:11 -0300

    feat: :sparkles: Add zustand store for state management to the app, utils folder for price validationes and created cart component

---
+++ app/components/cart/Cart.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image"
+
+import { useCartStore } from "@/store/useCartStore"
+import formatPrice from "@/utils/formatPrice"
+import { totalPrice } from "@/utils/totalPrice"
+
+
+const Cart = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default Cart
---
+++ utils/totalPrice.ts
@@ -0,0 +1,6 @@
+export const totalPrice = (cart) => {
+  return cart.reduce((acc, item) => {
+    return acc + item.unit_amount! * item.quantity!;
+  }, 0);
+};
+
