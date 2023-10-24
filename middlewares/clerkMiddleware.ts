5e9034c88c03db560a729da477710c85a646f475
EstebanPastor <estebanandrespastor@gmail.com>
2023-10-24 15:44:07 -0300

    fix: :lock: Fix user login/logout
    Now the issue that the user log out into the page and the log in modal pop-up is fixed, now isn't show anymore when the user log out

--- middlewares/clerkMiddleware.ts
+++ middlewares/clerkMiddleware.ts
@@ -2,7 +2,7 @@ import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
   ignoredRoutes: ["/((?!api|trpc))(_next|.+..+)(.*)"],
-  publicRoutes: ["/api/create-payment-intent,"],
+  publicRoutes: ["/api/create-payment-intent", "/"],
 });
 
 export const config = {
